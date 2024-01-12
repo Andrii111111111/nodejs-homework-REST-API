178d0cecedcd2e5adf953822122c2edde2716adf
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2024-01-12 22:26:49 +0100

    update

---
+++ controllers/Contacts/deleteById.js
@@ -0,0 +1,16 @@
+import { HttpError } from "../../helpers/index.js";
+import Contact from "../../models/Contact.js";
+
+const deleteById = async (req, res) => {
+  const { id } = req.params;
+  const { _id: owner } = req.user;
+  const result = await Contact.findOneAndDelete({ _id: id, owner });
+  if (!result) {
+    throw HttpError(404, `Contact with id=${id} not found`);
+  }
+  res.json({
+    message: "Delete success",
+  });
+};
+
+export default deleteById;
---
+++ routes/api/auth-router.js
@@ -0,0 +1,30 @@
+import express from "express";
+import authController from "../../controllers/Users/auth-controller.js";
+import { isEmptyBody, authenticate } from "../../middlewares/index.js";
+import { validateBody } from "../../decorators/index.js";
+import { userSignupSchema, userSigninSchema } from "../../models/User.js";
+import updateAvatar from "../../controllers/Users/updateAvatar.js";
+
+const authRouter = express.Router();
+
+authRouter.post(
+  "/register",
+  isEmptyBody,
+  validateBody(userSignupSchema),
+  authController.register
+);
+
+authRouter.post(
+  "/login",
+  isEmptyBody,
+  validateBody(userSigninSchema),
+  authController.signin
+);
+
+authRouter.get("/current", authenticate, authController.getCurrent);
+
+authRouter.post("/logout", authenticate, authController.signout);
+
+authRouter.patch("/avatars", authenticate, updateAvatar);
+
+export default authRouter;
